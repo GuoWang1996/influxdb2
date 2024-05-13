a93ecdcc9436182a58a429a774545d158d1b9b06
郭旺(44001539) <76993880@qq.com>
2024-05-13 13:40:29 +0800

    demo

---
+++ src/main/java/com/example/springboot_influxdb2/SpringbootInfluxdb2Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot_influxdb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootInfluxdb2Application {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootInfluxdb2Application.class, args);
+	}
+	
+}
---
+++ src/test/java/com/example/springboot_influxdb2/SpringbootInfluxdb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_influxdb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootInfluxdb2ApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
